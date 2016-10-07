66b56160d4677353855f3a1184d0c779e242d3b9
maiconwillianr <maiconwillianr@gmail.com>
2016-10-07 15:19:51 -0300

    Inclusão dos trailers na tela detalhes do filme

---
+++ app/src/main/java/br/com/maiconribeiro/popularmovies/VideoListFragment.java
@@ -0,0 +1,78 @@
+package br.com.maiconribeiro.popularmovies;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.View;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+import br.com.maiconribeiro.popularmovies.adapters.VideoListAdapter;
+import br.com.maiconribeiro.popularmovies.model.Video;
+
+/***********************************************************************************
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2015 Scott Cooper
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ***********************************************************************************/
+
+public class VideoListFragment extends ListFragment {
+
+    private ArrayList<Video> videos;
+
+    /**
+     * Empty constructor
+     */
+    public VideoListFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle args = getArguments();
+        this.videos = args.getParcelableArrayList("videos");
+        setListAdapter(new VideoListAdapter(getActivity(), videos));
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+
+        final Context context = getActivity();
+        final String DEVELOPER_KEY = BuildConfig.THE_GOOGLE_API_KEY;
+        final Video video = videos.get(position);
+
+
+        //Opens in the the custom Lightbox activity
+        final Intent lightboxIntent = new Intent(context, CustomLightboxActivity.class);
+        lightboxIntent.putExtra(CustomLightboxActivity.KEY_VIDEO_ID, video.getKey());
+        startActivity(lightboxIntent);
+
+    }
+
+}
---
+++ app/src/main/java/br/com/maiconribeiro/popularmovies/adapters/VideoListAdapter.java
@@ -0,0 +1,154 @@
+package br.com.maiconribeiro.popularmovies.adapters;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubeThumbnailLoader;
+import com.google.android.youtube.player.YouTubeThumbnailView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import br.com.maiconribeiro.popularmovies.BuildConfig;
+import br.com.maiconribeiro.popularmovies.R;
+import br.com.maiconribeiro.popularmovies.model.Video;
+
+/***********************************************************************************
+ * The MIT License (MIT)
+ * <p/>
+ * Copyright (c) 2015 Scott Cooper
+ * <p/>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p/>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p/>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ***********************************************************************************/
+
+public class VideoListAdapter extends BaseAdapter implements YouTubeThumbnailView.OnInitializedListener {
+
+    private Context mContext;
+    private Map<View, YouTubeThumbnailLoader> mLoaders;
+    private List<Video> videos;
+
+    public VideoListAdapter(final Context context, List<Video> videos) {
+        mContext = context;
+        mLoaders = new HashMap<>();
+        this.videos = videos;
+    }
+
+    @Override
+    public int getCount() {
+        return videos.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return videos.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        VideoHolder holder;
+
+        final Video video = videos.get(position);
+
+        if (convertView == null) {
+            //Create the row
+            final LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.row_video_layout, parent, false);
+
+            //Create the video holder
+            holder = new VideoHolder();
+
+            //Set the title
+            holder.title = (TextView) convertView.findViewById(R.id.textView_title);
+            holder.title.setText(video.getName());
+
+            //Initialise the thumbnail
+            holder.thumb = (YouTubeThumbnailView) convertView.findViewById(R.id.imageView_thumbnail);
+            holder.thumb.setTag(video.getKey());
+            holder.thumb.initialize(BuildConfig.THE_GOOGLE_API_KEY, this);
+
+            convertView.setTag(holder);
+        } else {
+            //Create it again
+            holder = (VideoHolder) convertView.getTag();
+            final YouTubeThumbnailLoader loader = mLoaders.get(holder.thumb);
+
+            if (video != null) {
+                //Set the title
+                holder.title.setText(video.getName());
+
+                //Setting the video id can take a while to actually change the image
+                //in the meantime the old image is shown.
+                //Removing the image will cause the background color to show instead, not ideal
+                //but preferable to flickering images.
+                holder.thumb.setImageBitmap(null);
+
+                if (loader == null) {
+                    //Loader is currently initialising
+                    holder.thumb.setTag(video.getKey());
+                } else {
+                    //The loader is already initialised
+                    //Note that it's possible to get a DeadObjectException here
+                    try {
+                        loader.setVideo(video.getIdVideo());
+                    } catch (IllegalStateException exception) {
+                        //If the Loader has been released then remove it from the map and re-init
+                        mLoaders.remove(holder.thumb);
+                        holder.thumb.initialize(BuildConfig.THE_GOOGLE_API_KEY, this);
+
+                    }
+                }
+
+            }
+        }
+        return convertView;
+    }
+
+
+    @Override
+    public void onInitializationSuccess(YouTubeThumbnailView view, YouTubeThumbnailLoader loader) {
+        mLoaders.put(view, loader);
+        loader.setVideo((String) view.getTag());
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubeThumbnailView thumbnailView, YouTubeInitializationResult errorReason) {
+        final String errorMessage = errorReason.toString();
+        Toast.makeText(mContext, errorMessage, Toast.LENGTH_LONG).show();
+    }
+
+
+    static class VideoHolder {
+        YouTubeThumbnailView thumb;
+        TextView title;
+    }
+}
